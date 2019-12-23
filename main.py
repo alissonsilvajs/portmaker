d9d5cf299b2ab70de3a433d5d6ab8307650ec251
Alisson Silva <alissonsilva.py@gmail.com>
2019-12-23 00:29:17 -0300

    v1.1

--- main.py
+++ main.py
@@ -1,12 +1,15 @@
 import Algorithmia
 from google_images_search import GoogleImagesSearch
+from progress.bar import ShadyBar
+import json
 from jinja2 import Environment, FileSystemLoader
 from weasyprint import HTML
 
-gis = GoogleImagesSearch('AIzaSyB4hmYFpThrSIhn8idx94Z67w7zi6VWn6Q', '002280090757924293604:fs75zw78et9')
-client = Algorithmia.client('simNstF6swzH7mgCRl6i9AWP3t91')
 env = Environment(loader=FileSystemLoader('.'))
 template = env.get_template("index.html")
+
+gis = GoogleImagesSearch('AIzaSyB4hmYFpThrSIhn8idx94Z67w7zi6VWn6Q', '002280090757924293604:fs75zw78et9')
+client = Algorithmia.client('simNstF6swzH7mgCRl6i9AWP3t91')
 
 def searchArticle(article):
 
@@ -49,7 +52,6 @@ def searchArticle(article):
             findDot = findDot + 1 
 
         resultReplaced = cutReplaceA + result[cutLineB:findDot]
-    print(resultReplaced)
     return resultReplaced
 
 def searchImageArticle(imageArticle):
@@ -60,24 +62,46 @@ def searchImageArticle(imageArticle):
     for image in gis.results():
         url = image.url
 
-    print("Image URL: " + url)
     return url
 
-searchImageArticle("Geometria")
-article = "Geometria"
-content = searchArticle(article)
+array = ['Geometria', 'Aquecimento Global']
+maxbar = len(array)
+bar = ShadyBar('Processing', max=maxbar)
+
+arrayImage = []
+arrayArticle = []
+
+for x in array:
+    resultImage = searchImageArticle(x)
+    arrayImage.append(resultImage)
+    resultArticle = searchArticle(x)
+    arrayArticle.append(resultArticle)
+
+    bar.next()
+    pass 
+bar.finish()
 
 portfolio_id = "Humanas"
 portfolio_name = "Alisson" 
 
-portfolio_article1 = article
-portfolio_content1 = content
 getVars = {
-    'title': portfolio_id, 
+    'title': portfolio_id,
     'name': portfolio_name, 
-    'article1': portfolio_article1, 
-    'content1': portfolio_content1,
 }
+
+lenArray = len(arrayImage)
+
+for x in range(lenArray): 
+
+    content = "content" + str(x)
+    getVars[content] = arrayArticle[x]
+    article = "article" + str(x)
+    getVars[article] = array[x]
+    image = "image" + str(x)
+    getVars[image] = arrayImage[x]    
+    pass
+
+print(getVars)
 
 html_out = template.render(getVars)
 HTML(string=html_out).write_pdf("report.pdf")
